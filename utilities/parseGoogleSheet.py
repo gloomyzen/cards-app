954fd282f94649e2713c20d2e84d3c6e57785a1f
Artur Barilo <faustkeller45@gmail.com>
2021-01-27 00:19:28 +0900

    master | wip: parse google sheet

--- utilities/parseGoogleSheet.py
+++ utilities/parseGoogleSheet.py
@@ -8,15 +8,32 @@ import json
 
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 
-SAMPLE_SPREADSHEET_ID = ''
-# SAMPLE_RANGE_NAME = 'book1!A2:G'
+SPREADSHEET_ID = ''
+START_ROW = 1
+RANGE_NAME = 'book1!A2:G'  # todo fill this data dynamic!
 
-def initGeneralSettings():
+def getSpreadSheetId():
     with open('parseSettings.json') as json_file:
         data = json.load(json_file)
-        print('spreadsheetId: ' + data['spreadsheetId'])
+        if not data['spreadsheetId']:
+            print("spreadsheetId is not valid!")
+        return data['spreadsheetId']
 
-def main():
+def getStartRow():
+    with open('parseSettings.json') as json_file:
+        data = json.load(json_file)
+        if not data['startRow']:
+            print("startRow is not valid!")
+        return data['startRow']
+
+def parseSheet(spreadsheetId, startRow):
+    with open('parseSettings.json') as json_file:
+        data = json.load(json_file)
+    for page in data['pages']:
+        print('sheet: ' + page['sheet'])
+        print('output: ' + page['output'])
+
+def parseData():
     creds = None
     if os.path.exists('token.pickle'):
         with open('token.pickle', 'rb') as token:
@@ -35,16 +52,23 @@ def main():
 
     # Call the Sheets API
     sheet = service.spreadsheets()
-    result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-                                range=SAMPLE_RANGE_NAME).execute()
+    result = sheet.values().get(spreadsheetId=SPREADSHEET_ID,
+                                range=RANGE_NAME).execute()
     values = result.get('values', [])
 
-    # if not values:
-    #     print('No data found.')
-    # else:
-    #     print('Name, Major:')
-    #     for row in values:
-    #         print('%s, %s' % (row[0], row[4]))
+    if not values:
+        print('No data found.')
+    else:
+        print('Name, Major:')
+        for row in values:
+            print('%s, %s' % (row[0], row[4]))
+
+def main():
+    spreadsheetId = getSpreadSheetId()
+    # print('%s' % (spreadsheetId))
+    startRow = getStartRow()
+    # print('%s' % (startRow))
+    parseSheet(spreadsheetId, startRow)
 
 if __name__ == '__main__':
     main()
