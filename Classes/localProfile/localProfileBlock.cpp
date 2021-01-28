7ccf595f9be26d62902b3748779dad2e9aecefda
Artur Barilo <faustkeller45@gmail.com>
2021-01-28 21:38:04 +0900

    master | wip: update profile manager, local profile

--- Classes/localProfile/localProfileBlock.cpp
+++ Classes/localProfile/localProfileBlock.cpp
@@ -8,11 +8,21 @@ localProfileBlock::localProfileBlock() {}
 localProfileBlock::~localProfileBlock() {}
 
 bool localProfileBlock::load(const rapidjson::GenericValue<rapidjson::UTF8<char>>::ConstObject& data) {
-	auto test = data.FindMember("courses");
-	auto test2 = test->name.GetString();
-	auto test3 = test->value.IsArray();
-	auto test4 = "";
-	return false;
+	auto coursesArray = data.FindMember("courses");
+	if (!coursesArray->value.IsArray()) {
+		return true;
+	}
+	auto courses = coursesArray->value.GetArray();
+	for (auto it = courses.Begin(); it != courses.End(); ++it) {
+		if (it->IsObject()) {
+			auto item = new sLocalProfileCourse();
+			if (item->load(it->GetObjectJ())) {
+				localCourses.insert({item->id, item});
+			}
+		}
+
+	}
+	return true;
 }
 
 bool localProfileBlock::save(rapidjson::GenericValue<rapidjson::UTF8<char>>::ConstObject& data) {
@@ -20,5 +30,17 @@ bool localProfileBlock::save(rapidjson::GenericValue<rapidjson::UTF8<char>>::Con
 }
 
 bool sLocalProfileCourse::load(const rapidjson::GenericValue<rapidjson::UTF8<char>>::ConstObject& data) {
+	auto _id = data.FindMember("id");
+	auto _goodQuestion = data.FindMember("goodQuestion");
+	auto _mediumQuestion = data.FindMember("mediumQuestion");
+	auto _badQuestion = data.FindMember("badQuestion");
+	if (_id != data.MemberEnd() && _id->value.IsInt()) {
+		id = _id->value.GetInt();
+	} else {
+		return false;
+	}
+	if (_goodQuestion != data.MemberEnd() && _goodQuestion->value.IsArray()) {
+//		id = _id->value.GetInt();
+	}
 	return false;
 }
