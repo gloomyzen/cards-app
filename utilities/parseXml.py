334cf9abd748918d3df6fe5f09cd79c6564cc805
Artur Barilo <faustkeller45@gmail.com>
2021-01-31 19:23:12 +0900

    (feature)master | wip: letter ipa database

---
+++ utilities/parseXml.py
@@ -0,0 +1,20 @@
+import os.path
+import xmltodict, json
+from xml.dom import minidom
+
+def main():
+    dictionary = {'a': [], 'b': [], 'c': [], 'd': [], 'e': [], 'f': [], 'g': [], 'h': [], 'i': [], 'j': [], 'k': [], 'l': [], 'm': [], 'n': [], 'o': [], 'p': [], 'q': [], 'r': [], 's': [], 't': [], 'u': [], 'v': [], 'w': [], 'x': [], 'y': [], 'z': []}
+    xmldoc = minidom.parse('ipa.xml')
+    itemlist = xmldoc.getElementsByTagName('ss:Row')
+    for s in itemlist:
+        item = s.getElementsByTagName('Data')
+        key = item[0].firstChild.data
+        val = item[1].firstChild.data
+        dictionary[key[0].lower()].append({'key': key, 'val': val})
+        # print(item[0].firstChild.data)
+    for letter in dictionary:
+        with open('../Resources/properties/database/dictionary/'+ letter + '.json', 'w') as outfile:
+            json.dump(dictionary[letter], outfile)
+
+if __name__ == '__main__':
+    main()
