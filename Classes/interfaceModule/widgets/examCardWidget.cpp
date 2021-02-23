68f1b6da52571a4ea497bab013e3ff4694c2caa3
Artur Barilo <faustkeller45@gmail.com>
2021-02-24 01:15:23 +0900

    (feature)master | wip features with window system - wip examCard wnd examWindow - front card view

--- Classes/interfaceModule/widgets/examCardWidget.cpp
+++ Classes/interfaceModule/widgets/examCardWidget.cpp
@@ -1,5 +1,6 @@
 #include "examCardWidget.h"
 #include "common/coreModule/nodes/widgets/gridNode.h"
+#include <cctype>
 
 using namespace cardsApp::interfaceModule;
 
@@ -9,6 +10,26 @@ examCardWidget::examCardWidget() {
 }
 
 void examCardWidget::setData(int, cardsApp::databasesModule::sCourseCard* card) {
+	auto capitalize = [](std::string str){
+		if (str.empty()) {
+			return str;
+		}
+		std::transform(str.begin(), str.end(), str.begin(), [](unsigned char c){
+			return std::tolower(c);
+		});
+//		str[0] = static_cast<std::string::value_type>(toupper(str[0]));
+		return str;
+	};
 	auto grid = dynamic_cast<common::coreModule::gridNode*>(findNode("gridContainer"));
+	if (auto label = dynamic_cast<cocos2d::Label*>(findNode("firstWord"))) {
+		label->setString(capitalize(card->ruWord));
+	}
+	if (!card->ruSentence.empty()) {
+		auto label = new cocos2d::Label();
+		label->setName("label");
+		loadComponent("widgets/" + this->getName(), label);
+		label->setString(capitalize(card->ruSentence));
+		grid->addChild(label);
+	}
 	grid->updateGridTransform();
 }
