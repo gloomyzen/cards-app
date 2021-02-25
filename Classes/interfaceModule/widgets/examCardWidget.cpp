fc2c0cadc77ed44c1fa95f0b2927d6e9b7b8497e
Artur Barilo <faustkeller45@gmail.com>
2021-02-25 21:37:38 +0900

    (feature)master | wip features with window system - wip examCard wnd examWindow - front card view - string format with boost

--- Classes/interfaceModule/widgets/examCardWidget.cpp
+++ Classes/interfaceModule/widgets/examCardWidget.cpp
@@ -1,6 +1,8 @@
 #include "examCardWidget.h"
 #include "common/coreModule/nodes/widgets/gridNode.h"
 #include <boost/locale.hpp>
+#include <boost/algorithm/string.hpp>
+#include <boost/iterator/filter_iterator.hpp>
 
 using namespace cardsApp::interfaceModule;
 
@@ -10,13 +12,23 @@ examCardWidget::examCardWidget() {
 }
 
 void examCardWidget::setData(int, cardsApp::databasesModule::sCourseCard* card) {
-	auto capitalize = [](std::string line)  {
+	auto capitalize = [](const std::string& line) {
+		std::string result;
 		boost::locale::generator gen;
 		std::locale loc=gen("");
 		std::locale::global(loc);
 		std::cout.imbue(loc);
-		line = boost::locale::to_title(line);
-		return line;
+		std::vector<std::string> splitVec;
+		split( splitVec, line, boost::is_any_of(" "), boost::token_compress_on);
+		for (auto it = splitVec.begin(); it != splitVec.end(); ++it) {
+			if (it == splitVec.begin()) {
+				result += STRING_FORMAT(" %s", boost::locale::to_title(*it).c_str());
+			} else {
+				result += STRING_FORMAT(" %s", boost::locale::to_lower(*it).c_str());
+			}
+		}
+
+		return result;
 	};
 	auto grid = dynamic_cast<common::coreModule::gridNode*>(findNode("gridContainer"));
 	if (auto label = dynamic_cast<cocos2d::Label*>(findNode("firstWord"))) {
