818c638e313697cfdce345b8077964d9f29c73b6
Artur Barilo <faustkeller45@gmail.com>
2021-02-27 00:15:27 +0900

    (feature)master | wip features with window system - wip examCard wnd examWindow - front card view - try to link boost to cocos2d

--- Classes/interfaceModule/widgets/examCardWidget.cpp
+++ Classes/interfaceModule/widgets/examCardWidget.cpp
@@ -19,22 +19,21 @@ examCardWidget::examCardWidget() {
 
 void examCardWidget::setData(int, cardsApp::databasesModule::sCourseCard* card) {
 	auto capitalize = [](const std::string& line) {
-		std::string result;
 		boost::locale::generator gen;
-		std::locale loc=gen("");
+		std::locale loc = gen("");
 		std::locale::global(loc);
 		std::cout.imbue(loc);
 		std::vector<std::string> splitVec;
 		split( splitVec, line, boost::is_any_of(" "), boost::token_compress_on);
-		for (auto it = splitVec.begin(); it != splitVec.end(); ++it) {
-			if (it == splitVec.begin()) {
-				result += STRING_FORMAT(" %s", boost::locale::to_title(*it).c_str());
-			} else {
-				result += STRING_FORMAT(" %s", (*it).c_str());
-			}
+		if (splitVec.size() == 1) {
+			std::string result;
+			result = splitVec.front();
+			boost::trim(result);
+			result = boost::locale::to_upper(result);
+			return result;
+		} else {
+			return line;
 		}
-
-		return result;
 	};
 	auto grid = dynamic_cast<common::coreModule::gridNode*>(findNode("gridContainer"));
 	if (auto label = dynamic_cast<cocos2d::Label*>(findNode("firstWord"))) {
--- lib/project_dependency.cmake
+++ lib/project_dependency.cmake
@@ -81,11 +81,14 @@ target_link_libraries(project_dependency INTERFACE dragonbones_target)
 #                               Boost
 #------------------------------------------------------------------------------
 #set (Boost_DEBUG ON)
-set (Boost_ARCHITECTURE "-x32 -arm -x86")
+set (Boost_ARCHITECTURE "-x32 -arm64 -x86 -armv7")
 set (Boost_USE_STATIC_LIBS ON)
 set (Boost_USE_MULTITHREADED ON)
 set (Boost_DETAILED_FAILURE_MSG ON)
-set (BOOST_ROOT "/usr/local/Cellar/boost")
+#if (ANDROID OR IOS)
+    set (Boost_INCLUDE_DIR "/usr/local/boost-1.75.0/include")
+    set (Boost_LIBRARY_DIR "/usr/local/boost-1.75.0/lib")
+#endif()
 find_package(Boost 1.46.1 REQUIRED COMPONENTS filesystem system locale)
 if(Boost_FOUND)
     include_directories(${Boost_INCLUDE_DIR})
@@ -94,7 +97,7 @@ if(Boost_FOUND)
 else()
     message (FATAL_ERROR "Cannot find Boost")
 endif()
-target_link_libraries(project_dependency INTERFACE ${Boost_LIBRARIES})
+target_link_libraries(project_dependency PRIVATE ${Boost_LIBRARIES})
 
 #------------------------------------------------------------------------------
 #                               Build Interface for all dependency
