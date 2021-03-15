4185ca99bf338d4ec0302cf22ecddb20695bb7f8
Artur Barilo <faustkeller45@gmail.com>
2021-03-16 00:01:13 +0900

    (feature)master | updates tests -wip  test ipa database

--- tests/TestDatabases.cpp
+++ tests/TestDatabases.cpp
@@ -1,15 +1,23 @@
-#include "../src/common/utilityModule/stringUtility.h"
+#include "common/databaseModule/databaseInterface.h"
+#include "common/databaseModule/databaseManager.h"
+#include "common/utilityModule/stringUtility.h"
+//#include "databasesModule/coursesDatabase.h"
 #include "cocos2d.h"
+#include "databasesModule/ipaDatabase.h"
 #include <gtest/gtest.h>
 
-int main(int argc, char **argv) {
+using namespace common::utilityModule;
+using namespace common::databaseModule;
+using namespace cardsApp::databasesModule;
+
+int main(int argc, char** argv) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
 
 namespace DBTests::test {
     class TempClass : public testing::Test {};
-}
+}// namespace DBTests::test
 using namespace DBTests::test;
 
 TEST_F(TempClass, cocos2dSimpleIntegrationTest) {
@@ -19,8 +27,15 @@ TEST_F(TempClass, cocos2dSimpleIntegrationTest) {
     EXPECT_TRUE(size1.width != size2.width);
 }
 
-TEST_F(TempClass, cardsAppStringUtilsTest) {
-    using namespace common::utilityModule;
+TEST_F(TempClass, commonStringUtilsTest) {
     EXPECT_EQ(stringUtility::capitalizeString("test room is awesome!"), std::string("Test room is awesome!"));
     EXPECT_EQ(stringUtility::toLowerString("Test RoOm IS aweSome!"), std::string("test room is awesome!"));
+}
+
+TEST_F(TempClass, cardsAppDatabaseTest) {
+    GET_DATABASE_MANAGER().registerDatabase({ "ipaDb", "properties/database/dictionary/db.json" },
+                                            new cardsApp::databasesModule::ipaDatabase());
+    GET_DATABASE_MANAGER().executeLoadData();
+    auto ipaDb = GET_DATABASE_MANAGER().getDatabase<ipaDatabase>("ipaDb");
+//    EXPECT_TRUE(ipaDb->isLoaded());
 }
